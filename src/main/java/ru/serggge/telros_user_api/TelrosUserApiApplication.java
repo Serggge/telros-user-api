7fa97d6553ba60dce74badda0c586c0c20e16be2
Serggge <serggge69@yandex.ru>
2025-07-11 12:48:32 +0300

    first commit

---
+++ src/main/java/ru/serggge/telros_user_api/TelrosUserApiApplication.java
@@ -0,0 +1,13 @@
+package ru.serggge.telros_user_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelrosUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelrosUserApiApplication.class, args);
+	}
+
+}
